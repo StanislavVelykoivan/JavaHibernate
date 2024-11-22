a8c750c753a92db9c35e342ff7a732e1c4b32673
StanislavVelykoivan <velykoivanstas@gmail.com>
2024-11-22 22:46:46 +0100

    -

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] arr = {21,1,23,3,21,3,21,3};
-
-        for (int n : arr){
-            System.out.println(n);
-        }
-    }
-}
